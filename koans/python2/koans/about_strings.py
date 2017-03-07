058ad5205f8797cd58793464b9ebdf318a235b2d
Ezra Lee <elee@remedypartners.com>
2017-03-07 17:43:25 -0500

    shortens strings

--- koans/python2/koans/about_strings.py
+++ koans/python2/koans/about_strings.py
@@ -56,10 +56,6 @@ world!
         b = """Hello "world"."""
         self.assertEqual(__, (a == b))
 
-    def test_escaping_quotes_at_the_end_of_triple_quoted_string(self):
-        string = """Hello "world\""""
-        self.assertEqual(__, string)
-
     def test_plus_concatenates_strings(self):
         string = "Hello, " + "world"
         self.assertEqual(__, string)
@@ -67,29 +63,3 @@ world!
     def test_adjacent_strings_are_concatenated_automatically(self):
         string = "Hello" ", " "world"
         self.assertEqual(__, string)
-
-    def test_plus_will_not_modify_original_strings(self):
-        hi = "Hello, "
-        there = "world"
-        string = hi + there
-        self.assertEqual(__, hi)
-        self.assertEqual(__, there)
-
-    def test_plus_equals_will_append_to_end_of_string(self):
-        hi = "Hello, "
-        there = "world"
-        hi += there
-        self.assertEqual(__, hi)
-
-    def test_plus_equals_also_leaves_original_string_unmodified(self):
-        original = "Hello, "
-        hi = original
-        there = "world"
-        hi += there
-        self.assertEqual(__, original)
-
-    def test_most_strings_interpret_escape_characters(self):
-        string = "\n"
-        self.assertEqual('\n', string)
-        self.assertEqual("""\n""", string)
-        self.assertEqual(__, len(string))
