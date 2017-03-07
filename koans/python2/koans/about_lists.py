c25bfbb54af7c4e3c386c457dfaaf9a05567e747
Ezra Lee <elee@remedypartners.com>
2017-03-07 17:58:29 -0500

    short lists

--- koans/python2/koans/about_lists.py
+++ koans/python2/koans/about_lists.py
@@ -52,18 +52,6 @@ class AboutLists(Koan):
         self.assertEqual(__, noms[2:])
         self.assertEqual(__, noms[:2])
 
-    def test_lists_and_ranges(self):
-        self.assertEqual(list, type(range(5)))
-        self.assertEqual(__, range(5))
-        self.assertEqual(__, range(5, 9))
-
-    def test_ranges_with_steps(self):
-        self.assertEqual(__, range(5, 3, -1))
-        self.assertEqual(__, range(0, 8, 2))
-        self.assertEqual(__, range(1, 8, 3))
-        self.assertEqual(__, range(5, -7, -4))
-        self.assertEqual(__, range(5, -8, -4))
-
     def test_insertions(self):
         knight = ['you', 'shall', 'pass']
         knight.insert(2, 'not')
@@ -71,37 +59,3 @@ class AboutLists(Koan):
 
         knight.insert(0, 'Arthur')
         self.assertEqual(__, knight)
-
-    def test_popping_lists(self):
-        stack = [10, 20, 30, 40]
-        stack.append('last')
-
-        self.assertEqual(__, stack)
-
-        popped_value = stack.pop()
-        self.assertEqual(__, popped_value)
-        self.assertEqual(__, stack)
-
-        popped_value = stack.pop(1)
-        self.assertEqual(__, popped_value)
-        self.assertEqual(__, stack)
-
-        # Notice that there is a "pop" but no "push" in python?
-
-        # Part of the Python philosophy is that there ideally should be one and
-        # only one way of doing anything. A 'push' is the same as an 'append'.
-
-        # To learn more about this try typing "import this" from the python
-        # console... ;)
-
-    def test_use_deques_for_making_queues(self):
-        from collections import deque
-
-        queue = deque([1, 2])
-        queue.append('last')
-
-        self.assertEqual(__, list(queue))
-
-        popped_value = queue.popleft()
-        self.assertEqual(__, popped_value)
-        self.assertEqual(__, list(queue))
